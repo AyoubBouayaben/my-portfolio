37b37b9fcc8e153850e1106d0680d57fab32993e
Ayoub.Ben <101188241+AyoubBouayaben@users.noreply.github.com>
2025-07-24 18:21:43 +0200

    Update portfolio.js

--- src/portfolio.js
+++ src/portfolio.js
@@ -383,7 +383,7 @@ const contactInfo = {
   subtitle:
     "Discuss a project or just want to say hi? My Inbox is open for all.",
   number: "",
-  email_address: "Ayoub.bouayaben@gmail.com"
+  email_address: "Ayoub.bouayaben@gmail.com",
   display: false
 };
 
