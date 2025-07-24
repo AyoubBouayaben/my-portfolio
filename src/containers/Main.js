320405f7ae5dad51722286bce6e91ab2c49f8ec5
Ayoub.Ben <101188241+AyoubBouayaben@users.noreply.github.com>
2025-07-24 18:43:01 +0200

    Update Main.js

--- src/containers/Main.js
+++ src/containers/Main.js
@@ -16,12 +16,13 @@ import ScrollToTopButton from "./topbutton/Top";
 import Twitter from "./twitter-embed/twitter";
 import Profile from "./profile/Profile";
 import SplashScreen from "./splashScreen/SplashScreen";
-import {splashScreen} from "../portfolio";
-import {StyleProvider} from "../contexts/StyleContext";
-import {useLocalStorage} from "../hooks/useLocalStorage";
+import { splashScreen } from "../portfolio";
+import { StyleProvider } from "../contexts/StyleContext";
+import { useLocalStorage } from "../hooks/useLocalStorage";
 import "./Main.scss";
 import Contact from "./Contact";
 import Service from "./Service";
+
 
 
 const Main = () => {
